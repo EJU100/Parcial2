b1ab201d8f1ad314acae844ae25b618d969b4204
Usta Eren Julian <erenjulian.usta@students.bfh.ch>
2022-05-10 18:37:44 -0500

    Create mySQL Test scribt

---
+++ parcial2_a01760042Test.sql
@@ -0,0 +1,13 @@
+--
+-- Creación de base de datos `parcial2_A01760042` 
+--
+
+CREATE DATABASE parcial2_A01760042;
+
+show databases;
+
+USE parcial2_A01760042;
+
+--
+-- Estructura de tabla para la tabla `Alumnos`
+--
